848a1da028874885a4dd89ddd9076824fcfaa4f0
Chor <97508567+tanch-p@users.noreply.github.com>
2025-10-22 16:58:23 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+def main():
+    print("Hello from bible-telegram-bot!")
+
+
+if __name__ == "__main__":
+    main()
